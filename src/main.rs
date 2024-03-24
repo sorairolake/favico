da03bd6a537242653111e29929c057c123d18bd0
Shun Sakai <sorairolake@protonmail.ch>
2024-03-24 18:31:47 +0900

    Initialize Cargo package

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Shun Sakai
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// Lint levels of rustc.
+#![forbid(unsafe_code)]
+#![deny(missing_debug_implementations)]
+#![warn(rust_2018_idioms)]
+// Lint levels of Clippy.
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
+#![allow(clippy::multiple_crate_versions)]
+
+fn main() {
+    println!("Hello, world!");
+}
