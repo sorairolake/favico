a2f4aa0c1ed1b132130755a47ff955ed4d475310
Shun Sakai <sorairolake@protonmail.ch>
2026-01-29 04:57:04 +0900

    style: Set `hex_literal_case` to `Upper`

--- tests/generate.rs
+++ tests/generate.rs
@@ -130,7 +130,7 @@ fn generate_png_ico() {
         fs::read(out_dir.join("favicon.ico"))
             .unwrap()
             .windows(8)
-            .filter(|b| *b == [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a])
+            .filter(|b| *b == [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
             .count(),
         3
     );
